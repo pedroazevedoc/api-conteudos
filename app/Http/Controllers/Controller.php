a73f417fea95dd4dbd678513f3e44b92b561370c
Pedro Azevedo <pedroazvdo.8@gmail.com>
2026-03-28 15:38:04 -0300

    feat (controller): adicionar métodos de padronização de resposta da api
    - Respostas de erro e sucesso padronizadas

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,21 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    protected function handleSuccessResponse(string $message = 'Mensagem genérica de successo', $data = null, int $statusCode = 200)
+    {
+        return response()->json([
+            'status' => 'success',
+            'message' => $message,
+            'payload' => $data,
+        ], $statusCode);
+    }
+
+    protected function handleErrorResponse(string $message, $details = null, int $statusCode)
+    {
+        return response()->json([
+            'status' => 'error',
+            'message' => $message,
+            'details' => $details,
+        ], $statusCode);
+    }
 }
