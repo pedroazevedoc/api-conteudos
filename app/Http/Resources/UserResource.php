c289a2d097c31dd9845d9ebb58daa1b246872ac9
Pedro Azevedo <pedroazvdo.8@gmail.com>
2026-03-29 23:02:06 -0300

    feat (user): adicionar atributo de criação do usuário na userResource

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -16,7 +16,8 @@ class UserResource extends JsonResource
     {
         return [
             'name'  => $this->name,
-            'email' => $this->email
+            'email' => $this->email,
+            'created_at' => $this->created_at->toDateTimeString(),
         ];
     }
 }
